8ab0c6057f392a87abe1e3b4149964476ef2b3da
Elena <le.poltanova@yandex.ru>
2023-02-11 15:11:20 +0300

    step 2

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ install:
 
 gendiff -h:
 	node bin/gendiff.js -h
+
+.PHONY test
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .version('1.0.0')
+  .help('output usage information')
 
 program.parse();
