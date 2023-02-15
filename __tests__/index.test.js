6b84433f565c6ccc0fee0622595ae4c2d2c5faee
Elena <le.poltanova@yandex.ru>
2023-02-15 10:43:59 +0300

    step 4 tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,13 +9,21 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-describe('genDiff should work correctly', () => {
-  const jsonFile1 = getFixturePath('file1.json');
-  const jsonFile2 = getFixturePath('file2.json');
+// describe('genDiff should work correctly', () => {
+//   const jsonFile1 = getFixturePath('file1.json');
+//   const jsonFile2 = getFixturePath('file2.json');
   
-  const expectedJSON = readFile('expectedJSON.txt');
+//   const expectedJSON = readFile('expectedJSON.txt');
   
-  //test.each
+//   //test.each
   
-  expect(genDiff(jsonFile1, jsonFile2)).toBe(expectedJSON);
+//   expect(genDiff(jsonFile1, jsonFile2)).toBe(expectedJSON);
+// });
+
+test('file json', () => {
+  const filename1 = getFixturePath('file1.json');
+  const filename2 = getFixturePath('file2.json');
+  const resultName = readFile('expectedJSON.txt');
+  const result = fs.readFileSync(resultName, 'utf8');
+  expect(genDiff(filename1, filename2)).toBe(result);
 });
